eb9831a17facc4a11039239315f7d52e05d5614d
javier gonzalez escudero <jovotron54@gmail.com>
2018-07-18 19:27:20 +0200

    Revert "restructuración proyecto"
    This reverts commit 5e5a76c1d84ef7ffad9c4aeb83cfeccf3dcebc14.

---
+++ freeBirdsTfg/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  freeBirdsTfg
+//
+//  Created by javier gonzalez escudero on 18/7/18.
+//  Copyright © 2018 javier gonzalez escudero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
