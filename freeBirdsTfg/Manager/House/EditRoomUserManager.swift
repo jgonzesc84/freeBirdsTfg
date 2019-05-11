b5de40de9772c2f7819a40d2fa810d050fa2b62b
javier gonzalez escudero <jovotron54@gmail.com>
2019-05-12 00:44:16 +0200

    go parar arriba

---
+++ freeBirdsTfg/Manager/House/EditRoomUserManager.swift
@@ -0,0 +1,9 @@
+//
+//  EditRoomUserManager.swift
+//  freeBirdsTfg
+//
+//  Created by javier gonzalez escudero on 11/05/2019.
+//  Copyright © 2019 javier gonzalez escudero. All rights reserved.
+//
+
+import Foundation
---
+++ freeBirdsTfg/View Controllers/House Manager View Controller/HouseManagerRoomCollectionCellCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HouseManagerRoomCollectionCellCollectionViewCell.swift
+//  freeBirdsTfg
+//
+//  Created by javier gonzalez escudero on 11/05/2019.
+//  Copyright © 2019 javier gonzalez escudero. All rights reserved.
+//
+
+import UIKit
+
+class HouseManagerRoomCollectionCellCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
