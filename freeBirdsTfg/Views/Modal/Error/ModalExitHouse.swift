ed099d0cc622dcb7bada049e1cd1f3cb517fdb20
javier gonzalez escudero <jovotron54@gmail.com>
2019-06-20 00:54:31 +0200

    bien

---
+++ freeBirdsTfg/Views/Modal/Error/ModalExitHouse.swift
@@ -0,0 +1,21 @@
+//
+//  ModalExitHouse.swift
+//  freeBirdsTfg
+//
+//  Created by javier gonzalez escudero on 20/06/2019.
+//  Copyright © 2019 javier gonzalez escudero. All rights reserved.
+//
+
+import UIKit
+
+class ModalExitHouse: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
